aa03679e7a8b6e72545dcf1c196217012b4e7f54
Fernando <folivelima@gmail.com>
2023-01-24 00:57:41 -0300

    finished bfs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,50 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::collections::VecDeque;
+
+
 
 const QUEUE_LIMIT: usize = 1000;
+
+fn path_sum(path: &[usize], distance_matrix: &[Vec<f64>]) -> f64 {
+    path.iter()
+        .zip(path.iter().skip(1))
+        .map(|(a, b)| distance_matrix[*a][*b])
+        .sum()
+}
+
+fn bfs(distance_matrix: Vec<Vec<f64>>) -> (VecDeque<Vec<usize>>, f64, Vec<usize>){
+    let node_count = distance_matrix.len();
+    let mut best_path = vec![0];
+    let mut best_distance = std::f64::MAX;
+
+    let mut queue: VecDeque<Vec<usize>> = VecDeque::new();
+
+    while let Some(path) = queue.pop_front() {
+        if path.len() == node_count {
+            let distance = path_sum(&path, &distance_matrix);
+            if distance < best_distance {
+                best_distance = distance;
+                best_path = path;
+            }
+        } else {
+            for next in 0..node_count {
+                if !path.contains(&next) {
+                    let mut new_path = path.clone();
+                    new_path.push(next);
+                    if path_sum(&new_path, &distance_matrix) < best_distance && queue.len() < QUEUE_LIMIT {
+                        queue.push_back(new_path);
+                    }
+                    if queue.len() >= QUEUE_LIMIT {
+                        break;
+                    }
+                }
+            }
+        }
+    } 
+    (queue, best_distance, best_path)
+}
 
 fn tsp(distance_matrix: Vec<Vec<f64>>, thread_count: usize) -> (Vec<usize>, f64) {
     let node_count = distance_matrix.len();
